99077603cada0ae5a56485d8b9326edfb8d3e7ce
c4n <c4n2012@gmail.com>
2018-04-18 20:59:15 +0300

    function css_maker defined

--- mapper/urls.py
+++ mapper/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_dynamic_map, name='show_dynamic_map'),
-    path('list/', views.workspaces_list, name='workplaces_list'),
+    path('', views.show_dynamic_map),
+    path('map2R',views.map2R),
+    path('map_admin', views.show_dynamic_map),
+    path('list/', views.workspaces_list),
+
     path('test/', views.test, name=''),
 ]
--- mapper/views.py
+++ mapper/views.py
@@ -29,6 +29,33 @@ def show_dynamic_map(request):
         f.write(raw + '\n')
     return render(request, 'mapper/map.html', {'workspaces_set' : workspaces_set})
 
+def css_maker(cssfile,workspaces_set):
+    for workspace in workspaces_set:
+        raw = ".workspace-position-" 
+        raw += workspace.stage + str(workspace.id)
+        raw += " { transform: translate("
+        raw += str(workspace.xPos)
+        raw += "px, "
+        raw += str(workspace.yPos)
+        raw += "px) scale(1,1);background-position: 0px 0px;} "
+        cssfile.write(raw + '\n')
+        raw = ".workspace-position-"
+        raw += workspace.stage + str(workspace.id)
+        raw += ":hover{ transform: translate("
+        raw += str(workspace.xPos)
+        raw += "px, "
+        raw += str(workspace.yPos-5)
+        raw +="px) scale(1.1,1.2); opacity: 1;}"
+        cssfile.write(raw + '\n')
+     
+
+def map2R(request):
+    stage='2R'
+    workspaces_set = Workspace.objects.filter(stage__contains=stage).order_by('-xPos').order_by('yPos')
+    f = open('mapper/static/styles/dynamic.css', 'w')
+    css_maker(f,workspaces_set)
+    return render(request, 'mapper/map.html', {'workspaces_set' : workspaces_set})
+
 
 def test(request):
     workspaces_set = Workspace.objects.filter(stage__contains='2R').order_by('id')
