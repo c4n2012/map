fe45a444cb26d45a7ef632c1567617e98e47161e
c4n <c4n2012@gmail.com>
2018-04-19 22:14:46 +0300

    cssmaker changed

--- mapper/urls.py
+++ mapper/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_dynamic_map),
+    path('',views.map2R),
     path('map2R',views.map2R),
     path('map2R_admin', views.map2R_admin),
     path('list/', views.workspaces_list),
-
-    path('test/', views.test, name=''),
 ]
--- mapper/views.py
+++ mapper/views.py
@@ -7,29 +7,8 @@ def workspaces_list(request):
     workspaces = Workspace.objects.filter(stage__contains='2R').order_by('id')
     return render(request, 'mapper/workspaces_list.html', {'workspaces' : workspaces})
 
-def show_dynamic_map(request):
-    workspaces_set = Workspace.objects.filter(stage__contains='2R').order_by('-xPos').order_by('yPos')
-    f = open('mapper/static/styles/dynamic.css', 'w')
-    for workspace in workspaces_set:
-        raw = ".workspace-position-" 
-        raw += workspace.stage + str(workspace.id)
-        raw += " { transform: translate("
-        raw += str(workspace.xPos)
-        raw += "px, "
-        raw += str(workspace.yPos)
-        raw += "px) scale(1,1);background-position: 0px 0px;} "
-        f.write(raw + '\n')
-        raw = ".workspace-position-"
-        raw += workspace.stage + str(workspace.id)
-        raw += ":hover{ transform: translate("
-        raw += str(workspace.xPos)
-        raw += "px, "
-        raw += str(workspace.yPos-5)
-        raw +="px) scale(1.1,1.2); opacity: 1;}"
-        f.write(raw + '\n')
-    return render(request, 'mapper/map.html', {'workspaces_set' : workspaces_set})
-
-def css_maker(cssfile,workspaces_set):
+def css_maker(stage,workspaces_set):
+    cssfile = open('mapper/static/styles/dynamic'+stage+'.css', 'w')
     for workspace in workspaces_set:
         raw = ".workspace-position-" 
         raw += workspace.stage + str(workspace.id)
@@ -47,48 +26,23 @@ def css_maker(cssfile,workspaces_set):
         raw += str(workspace.yPos-5)
         raw +="px) scale(1.1,1.2); opacity: 1;}"
         cssfile.write(raw + '\n')
+    cssfile.close()
      
 
 def map2R(request):
     stage='2R'
     workspaces_set = Workspace.objects.filter(stage__contains=stage).order_by('-xPos').order_by('yPos')
-    f = open('mapper/static/styles/dynamic'+stage+'.css', 'w')
-    css_maker(f,workspaces_set)
+    css_maker(stage,workspaces_set)
     return render(request, 'mapper/map.html', {'workspaces_set' : workspaces_set})
 
 def map2R_admin(request):
     stage='2R'
     workspaces_set = Workspace.objects.filter(stage__contains=stage).order_by('-xPos').order_by('yPos')
-    f = open('mapper/static/styles/dynamic'+stage+'.css', 'w')
-    css_maker(f,workspaces_set)
+    css_maker(stage,workspaces_set)
     return render(request, 'mapper/map_admin.html', {'workspaces_set' : workspaces_set})
-
 
 def map2L(request):
     stage='2L'
     workspaces_set = Workspace.objects.filter(stage__contains=stage).order_by('-xPos').order_by('yPos')
-    f = open('mapper/static/styles/dynamic'+stage+'.css', 'w')
-    css_maker(f,workspaces_set)
+    css_maker(stage,workspaces_set)
     return render(request, 'mapper/map.html', {'workspaces_set' : workspaces_set})
-
-def test(request):
-    workspaces_set = Workspace.objects.filter(stage__contains='2R').order_by('id')
-    f = open('mapper/static/styles/dynamic.css', 'w')
-    for workspace in workspaces_set:
-        raw = ".workspace-position-"
-        raw += workspace.stage + str(workspace.id)
-        raw += " { transform: translate("
-        raw += str(workspace.xPos)
-        raw += "px, "
-        raw += str(workspace.yPos)
-        raw += "px) scale(1,1); background-position: 0px 0px; } "
-        f.write(raw + '\n')
-        raw = ".workspace-position-"
-        raw += workspace.stage + str(workspace.id)
-        raw += ":hover{ transform: translate("
-        raw += str(workspace.xPos)
-        raw += "px, "
-        raw += str(workspace.yPos-2)
-        raw +="px) scale(1.1,1.1); z-index: 100; opacity: 1; }"
-        f.write(raw + '\n')
-    return render(request, 'mapper/test.html', {'workspaces_set' : workspaces_set})
